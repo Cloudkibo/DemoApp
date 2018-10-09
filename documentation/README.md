91f813170524f158f9380357bf37e80d8eebfe1a
Imran Shoukat <ishoukat@localhost.localdomain>
2018-10-09 15:06:49 +0500

    initial setup #1 #2

---
+++ documentation/README.md
@@ -0,0 +1,6 @@
+# CloudKibo Demo App
+Link to design document:
+https://docs.google.com/document/d/1yMBFKvNYf9bBIDbNMGg3Bbzo_I6ffMJllcjv07G8dT0/edit#
+
+Description:
+This project will be a demo app to show customer information. Further explanation can be found on design document.
---
+++ server/app.js
@@ -0,0 +1,20 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development' // production
+
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./config/environment/index')
+
+const app = express()
+const httpApp = express()
+
+const appObj = (config.env === 'production' || config.env === 'staging') ? app : httpApp
+
+mongoose.connect(config.mongo.uri, config.mongo.options)
+
+require('./config/express')(appObj)
+require('./config/setup')(app, httpApp, config)
+require('./routes')(appObj)
+
+process.on('uncaughtException', function (exception) {
+  console.log(exception)
+})
